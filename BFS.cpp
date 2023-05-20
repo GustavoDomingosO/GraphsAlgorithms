824d6f9920804bb411fe2d3b32cfb7fe1c73519a
unknown <gustavodomingosdeoliveira@hotmail.com>
2023-05-19 23:41:42 -0300

    Changed the code so that it stores the distance and father of each vertices.

--- BFS.cpp
+++ BFS.cpp
@@ -4,22 +4,27 @@
 
 using namespace std;
 
-void bfs(vector<vector<int>>& graph, int startNode) {
+void bfs(vector<vector<int>>& graph, int startNode,int prev[],int dist[], int pai[]) {
     int numNodes = graph.size();
-    vector<bool> visited(numNodes, false);
+    //vector<bool> visited(numNodes, false);
     queue<int> q;
-
-    visited[startNode] = true;
+    int cont = 0;
+    prev[startNode] = cont++;
+    dist[startNode] = 0;
+    pai[startNode] = -1;
+    //visited[startNode] = true;
     q.push(startNode);
 
     while (!q.empty()) {
         int currentNode = q.front();
         q.pop();
-        cout << currentNode << " ";
+        cout << currentNode + 1 << " ";
 
         for (int i = 0; i < numNodes; i++) {
-            if (graph[currentNode][i] && !visited[i]) {
-                visited[i] = true;
+            if (graph[currentNode][i] && prev[i] == -1) {
+                prev[i]= cont++;
+                dist[i] = dist[currentNode] + 1;
+                pai[i] = currentNode;
                 q.push(i);
             }
         }
@@ -42,14 +47,35 @@ int main() {
         graph[u][v] = 1;
         graph[v][u] = 1; // Assuming an undirected graph
     }
+    int prev[numNodes];
+    int dist[numNodes];
+    int pai[numNodes];
 
-    int startNode;
-    cout << "Enter the starting node: ";
-    cin >> startNode;
+    for(int i = 0; i < numNodes; i++)
+    {
+        prev[i] = -1;
+        dist[i] = -1;
+        pai[i] = -1;
+    }
 
-    cout << "BFS traversal starting from node " << startNode << ": ";
-    bfs(graph, startNode);
+    cout << "BFS traversal starting from node " << 1 << ": ";
+    bfs(graph, 0, prev, dist, pai);
     cout << endl;
+    for(int i = 0; i < numNodes; i++)
+    {
+        cout << "Vertex " << i << " was visited in the " << prev[i] + 1  << " iteration." << endl; 
+    }
+
+    for(int i = 0; i < numNodes; i++)
+    {
+        cout << "Vertex " << i << " has a distance of " << dist[i]  << " vertices from starting vertex 0" << endl; 
+    }
+
+    for(int i = 0; i < numNodes; i++)
+    {
+        cout << "Vertex " << i << " is the son of vertex " << pai[i] << endl; 
+    }
+
 
     return 0;
 }
